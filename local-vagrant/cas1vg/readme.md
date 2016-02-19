73da99454ef496f03837b378d4d3b4163f5181ed
michael-conway <michael_conway@unc.edu>
2016-02-19 14:12:04 -0500

    #33 first cleanup leaving only lab stuff add sample inventory and group vars

--- local-vagrant/cas1vg/readme.md
+++
@@ -1,4 +0,0 @@
-## CAS/LDAP 
-
-192.168.50.10 cas.localvagrant.org
-
--- roles/condor-submitter/templates/group_config.js
+++
@@ -1,5 +0,0 @@
-GROUP_NAMES = batch_processing
-
-GROUP_QUOTA_batch_processing = "{{group_config}}"
-
-GROUP_AUTOREGROUP = FALSE
